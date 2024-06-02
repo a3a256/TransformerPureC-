037963c492e2c56d6d0becba0bc1c45045542c09
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-06-03 00:29:26 +0100

    added header file with linear algebra functions from MultiHead attention repo with pointwise addition of matrices

---
+++ include/linalg_ops.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <math.h>
+#include <random>
+#include <climits>
+#include <numeric>
+
+std::vector<std::vector<float>> matmul(std::vector<std::vector<float>> one, std::vector<std::vector<float>> two){
+    int i, j, k;
+    std::vector<float> temp;
+    std::vector<std::vector<float>> res;
+    float _sum = 0.0f;
+    for(i=0; i<one.size(); i++){
+        for(j=0; j<two[0].size(); j++){
+            _sum = 0.0f;
+            for(k=0; k<one[0].size(); k++){
+                _sum += one[i][k]*two[k][j];
+            }
+            temp.push_back(_sum);
+        }
+        res.push_back(temp);
+        std::vector<float>().swap(temp);
+    }
+    return res;
+}
+
+std::vector<std::vector<float>> softmax(std::vector<std::vector<float>> &mat){
+    float _sum;
+    int i, j;
+    for(i=0; i<mat.size(); i++){
+        _sum = 0.0f;
+        for(j=0; j<mat[i].size(); j++){
+            _sum += std::exp(mat[i][j]);
+        }
+        for(j=0; j<mat[i].size(); j++){
+            mat[i][j] = mat[i][j]/_sum;
+        }
+    }
+
+    return mat;
+}
+
+std::vector<std::vector<float>> transpose(std::vector<std::vector<float>> mat){
+    int i, j;
+    std::vector<std::vector<float>> res(mat[0].size(), std::vector<float>(mat.size(), 0.0f));
+    for(j=0; j<mat[0].size(); j++){
+        for(i=0; i<mat.size(); i++){
+            res[j][i] = mat[i][j];
+        }
+    }
+
+    return res;
+}
+
+float random_value(){
+    std::random_device seeder;
+    std::mt19937 rng(seeder());
+    std::uniform_int_distribution<long> gen(INT_MIN, INT_MAX);
+    return (float)gen(rng)/(float)RAND_MAX;
+}
+
+std::vector<std::vector<float>> add(std::vector<std::vector<float>> x, std::vector<std::vector<float>> y){
+    int i, j;
+    for(i=0; i<x.size(); i++){
+        for(j=0; j<x[i].size(); j++){
+            x[i][j] += y[i][j];
+        }
+    }
+    return x;
+}
