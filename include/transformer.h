ea6e3de31f3c241b6a1198e506f2dfb7b6b4d883
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-06-09 00:18:36 +0100

    implemented full encoder design

--- include/transformer.h
+++ include/transformer.h
@@ -34,49 +34,75 @@ class FeedForwardNetwork{
 };
 
 class EncoderLayer{
-    int embedding, heads;
-    MultiHeadAttention mha;
-    LayerNormalization norm1, norm2;
-    FeedForwardNetwork ffn;
-    EncoderLayer(int sequence_len, int em_size, int num_heads, int hidden_neurons){
-        embedding = em_size;
-        heads = num_heads;
-        mha = MultiHeadAttention(em_size, num_heads);
-        norm1 = LayerNormalization(sequence_len, em_size);
-        norm2 = LayerNormalization(sequence_len, em_size);
-        ffn = FeedForwardNetwork(em_size, hidden_neurons);
-    }
 
-    std::vector<std::vector<float>> forward(std::vector<std::vector<float>> x){
-        x = add(x, mha.forward(x, x, x));
-        x = norm1.forward(x);
-        x = add(x, ffn.forward(x));
-        x = norm2.forward(x);
+    public:
+        int embedding, heads;
+        MultiHeadAttention mha;
+        LayerNormalization norm1, norm2;
+        FeedForwardNetwork ffn;
+        EncoderLayer(int sequence_len, int em_size, int num_heads, int hidden_neurons){
+            embedding = em_size;
+            heads = num_heads;
+            mha = MultiHeadAttention(em_size, num_heads);
+            norm1 = LayerNormalization(sequence_len, em_size);
+            norm2 = LayerNormalization(sequence_len, em_size);
+            ffn = FeedForwardNetwork(em_size, hidden_neurons);
+        }
 
-        return x;
-    }
+        std::vector<std::vector<float>> forward(std::vector<std::vector<float>> x){
+            x = add(x, mha.forward(x, x, x));
+            x = norm1.forward(x);
+            x = add(x, ffn.forward(x));
+            x = norm2.forward(x);
+
+            return x;
+        }
 };
 
 class DecoderLayer{
-    MultiHeadAttention mha1, mha2;
-    LayerNormalization norm1, norm2, norm3;
-    FeedForwardNetwork ffn;
-    DecoderLayer(int sequence_len, int em_size, int num_heads, int hidden_neurons){
-        mha1 = MultiHeadAttention(em_size, num_heads);
-        mha2 = MultiHeadAttention(em_size, num_heads);
-        norm1 = LayerNormalization(sequence_len, em_size);
-        norm2 = LayerNormalization(sequence_len, em_size);
-        norm3 = LayerNormalization(sequence_len, em_size);
-        ffn = FeedForwardNetwork(em_size, hidden_neurons);
+
+    public:
+
+        MultiHeadAttention mha1, mha2;
+        LayerNormalization norm1, norm2, norm3;
+        FeedForwardNetwork ffn;
+        DecoderLayer(int sequence_len, int em_size, int num_heads, int hidden_neurons){
+            mha1 = MultiHeadAttention(em_size, num_heads);
+            mha2 = MultiHeadAttention(em_size, num_heads);
+            norm1 = LayerNormalization(sequence_len, em_size);
+            norm2 = LayerNormalization(sequence_len, em_size);
+            norm3 = LayerNormalization(sequence_len, em_size);
+            ffn = FeedForwardNetwork(em_size, hidden_neurons);
+        }
+
+        std::vector<std::vector<float>> forward(std::vector<std::vector<float>> x, std::vector<std::vector<float>> encoded){
+            x = add(x, mha1.forward(x, x, x));
+            x = norm1.forward(x);
+            x = add(x, mha2.forward(x, encoded, encoded));
+            x = norm2.forward(x);
+            x = add(x, ffn.forward(x));
+            x = norm3.forward(x);
+            return x;
+        }
+};
+
+class Encoder{
+
+    std::vector<EncoderLayer> enc_layers;
+    
+    Encoder(int sequence_len, int em_size, int num_heads, int hidden_neurons, int num_layers){
+        int i;
+        for(i=0; i<num_layers; i++){
+            EncoderLayer enc_layer(sequence_len, em_size, num_heads, hidden_neurons);
+            enc_layers.push_back(enc_layer);
+        }
     }
 
-    std::vector<std::vector<float>> forward(std::vector<std::vector<float>> x, std::vector<std::vector<float>> encoded){
-        x = add(x, mha1.forward(x, x, x));
-        x = norm1.forward(x);
-        x = add(x, mha2.forward(x, encoded, encoded));
-        x = norm2.forward(x);
-        x = add(x, ffn.forward(x));
-        x = norm3.forward(x);
+    std::vector<std::vector<float>> forward(std::vector<std::vector<float>> x){
+        int i;
+        for(i=0; i<enc_layers.size(); i++){
+            x = enc_layers[i].forward(x);
+        }
         return x;
     }
 };
